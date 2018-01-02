b04ee72c46f2dc28797cd5dced285a033d78b6ea
mountbicepius <mountbicepius@github.com>
2018-01-02 20:37:48 +0530

    Initial commit

---
+++ app/src/main/java/com/anirudh/novaorbis/verisystems/MainActivity.java
@@ -0,0 +1,13 @@
+package com.anirudh.novaorbis.verisystems;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
